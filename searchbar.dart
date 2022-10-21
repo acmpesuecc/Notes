ba3caa53655d4d96acac7deab598f4872d5e52e4
tnayagarwal <tanaynilesh@gmail.com>
2022-10-21 23:02:09 +0530

    search bar

---
+++ searchbar.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+ @override
+ Widget build(BuildContext context) {
+ return const MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: 'Material App',
+  home: HomeScreen(),
+ );
+ }
+}
+class HomeScreen extends StatefulWidget {
+ const HomeScreen({Key? key}) : super(key: key);
+ @override
+ _HomeScreenState createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+  appBar: AppBar(
+  title: const Text('Note that u want to add'),
+  automaticallyImplyLeading: false,
+  centerTitle: true,
+  ),
+  body: Column(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+   const Center(
+   child: Text('Hi'),
+   ),
+  ],
+  ),
+ );
+ }
+}
+appBar: AppBar(
+  title: const Text('Note that u want to add'),
+  automaticallyImplyLeading: false,
+  actions: [
+   IconButton(
+   onPressed: () {},
+   icon: const Icon(Icons.search),
+   )
+  ],
+  centerTitle: true,
+  ),
+  @override
+ _HomeScreenState createState() => _HomeScreenState();
+}
+class _HomeScreenState extends State<HomeScreen> {
+ Icon customIcon = const Icon(Icons.search);
+ Widget customSearchBar = const Text('Search for a note');
+ @override
+ Widget build(BuildContext context) {
+ return Scaffold(
+  appBar: AppBar(
+  title: customSearchBar,
+  automaticallyImplyLeading: false,
+  actions: [
+   IconButton(
+   onPressed: () {},
+   icon: customIcon,
+   )
+  ],
+  centerTitle: true,
+  ),
+  onPressed: () {
+    setState(() {
+    if (customIcon.icon == Icons.search) {
+     // Perform set of instructions.
+    } else {
+     customIcon = const Icon(Icons.search);
+     customSearchBar = const Text('Search for a note');
+    }
+    });
+   },
+   if (customIcon.icon == Icons.search) {
+   customIcon = const Icon(Icons.cancel);
+   customSearchBar = const ListTile(
+   leading: Icon(
+    Icons.search,
+    color: Colors.white,
+    size: 28,
+   ),
+   title: TextField(
+    decoration: InputDecoration(
+    hintText: 'Search for a note',
+    hintStyle: TextStyle(
+     color: Colors.white,
+     fontSize: 18,
+     fontStyle: FontStyle.italic,
+    ),
+    border: InputBorder.none,
+    ),
+    style: TextStyle(
+    color: Colors.white,
+    ),
+   ),
+   );
+  } 
