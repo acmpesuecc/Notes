26cf9816553cdd93441a1572ac66ab148c4ced01
PandaTGOS <sakhisaswatpanda@gmail.com>
2022-10-22 00:37:16 +0530

    Connited changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,28 @@
 import 'package:flutter/material.dart';
 import 'home.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.deepPurple,
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'No notes have been added!',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey,
+              fontFamily: 'Kalam',
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text('Hello'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Text('Delete'),
+          backgroundColor: Colors.red,
+        ),
       ),
-      home: Home(),
-    );
-  }
-}
+    ));
